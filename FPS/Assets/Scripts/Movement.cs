01f34abc2e7654809eef4f830441a0dd571c5ccf
Coolp-GitHub <mohanparasinder@gmail.com>
2021-01-07 19:36:59 -0500

    Added Player Movement
    Player movement:
    
    -Jumping
    -walking around
    -looking with camera

---
+++ FPS/Assets/Scripts/Movement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float jumpHeight = 1f;
+    public float speed = 12f;
+
+    Vector3 velocity;
+
+    public float grav = -9.81f * 3;
+
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+
+    bool isGrounded;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * grav);
+        }
+
+        velocity.y += grav * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
