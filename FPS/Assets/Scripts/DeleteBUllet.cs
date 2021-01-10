a703625e764e1385abcb142b7cd61b0411602b31
Coolp-GitHub <mohanparasinder@gmail.com>
2021-01-09 21:28:28 -0500

    More Updates
    Added:
    
    - g u n
    
    -bullets get deleted when they hit objects tagged with "Enemy"

---
+++ FPS/Assets/Scripts/DeleteBUllet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteBUllet : MonoBehaviour
+{
+    public GameObject bullet;
+
+    void OnCollisionEnter(Collision ci)
+    {
+        if (ci.collider.tag == "Enemy")
+        {
+            Destroy(bullet);
+        }
+    }
+
+    
+}
---
+++ FPS/Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    public bool canShoot = true;
+    public float cooldown = 0.5f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && canShoot)
+        {
+            Shooting();
+            canShoot = false;
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    void Shooting()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.AddForce(firePoint.up * 20, ForceMode.Impulse);
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
+    }
+}
