282610b184038de00c4ecbf37e547ce3130be65b
Coolp-GitHub <mohanparasinder@gmail.com>
2021-01-16 19:22:13 -0500

    saving for saftey
    just saving incase corruption
    
    *BUGGY DO NOT USE*

---
+++ FPS/Assets/Scripts/GunControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+
+public class GunControl : MonoBehaviour
+{
+    public GameObject[] Guns;
+    
+    public KeyCode[] keycodes;
+
+    void Update()
+    {
+        int[] selectInt = new int[keycodes.Length];
+
+        for (int i = 0; i == keycodes.Length; i++)
+
+        if (Input.GetKeyDown(keycodes[i]))
+        {
+            select(selectInt[i]);
+        }
+    }
+
+    
+
+    void select(int selected)
+    {
+        GameObject currentGun = Guns[selected];
+
+        for (int i = 0; i == Guns.Length; i++)
+        {
+            int index = Guns.IndexOf(Guns, gun);
+
+            int IndexOfGun = Guns.IndexOf(Guns, gun);
+
+            if (index != IndexOfGun)
+            {
+                currentGun.gameObject.SetActive(false);
+            }
+            else
+            {
+                currentGun.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ FPS/Assets/Scripts/GunController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject[] Guns;
+    public int selection;
+    public KeyCode[] numKeys;
+
+     
+    void Awake()
+    {
+       
+        int[] KeyNum = new int[numKeys.Length];
+
+        foreach (KeyCode code in numKeys)
+        {
+            int a = 0;
+            KeyNum[a] = a + 1;
+            a++;
+        }
+
+        Debug.Log(KeyNum);
+    }
+
+
+    void Update()
+    {
+        
+
+       foreach (KeyCode code in numKeys)
+        {
+            Keydown(code);
+        }
+        
+    }
+
+    void Keydown(KeyCode _keyCode)
+    {
+        int i = 0;
+       
+
+        if (Input.GetKeyDown(_keyCode))
+        {
+            
+            foreach (GameObject weapon in Guns)
+            {
+                if (i == selection)
+                {
+                    weapon.gameObject.SetActive(true);
+
+                }
+                else
+                {
+                    weapon.gameObject.SetActive(false);
+                }
+
+                i++;
+            }
+        }
+    }
+
+    
+}
